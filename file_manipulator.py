400362b2579b1446294d82654ae6f5a2e15f9679
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-07 12:09:04 +0530

    Added more things with write method :)

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,16 +1,43 @@
 """This is a File Manipulator programme"""
 
+def divider(n = 100):
+    print("-" * n)
+
 try:
     file = open("FileManipulator/readfile.txt", "x")
     print("Read file has been made.")
+    divider()
     file.close()
 except FileExistsError:
     print("File already Exists. Skipping this ;)")
-    print("-" * 100)
+    divider()
 
 try:
-    file = open("FileManipulator/filerad.txt", "r")
+    file = open("FileManipulator/readfile.txt", "r")
     print(file.read())
+    divider()
     file.close()
 except FileNotFoundError:
     print("You forgot to make the file my boy :) or you typed the name wrong ;)")
+    divider()
+
+try:
+    file = open("FileManipulator/writefile.txt", "w")
+    text = "Hello I am Skele.\nAnd I am writing few things in the file.\nI guess You will like this.\n:) ;) :-) idk ...\nHello and Bye."
+
+    file.write(text)
+    print("File has been written.")
+    divider()
+    file.close()
+except Exception as e:
+    print(f"An error occured.\n{e}")
+    divider()
+
+try:
+    file = open("FileManipulator/writefile.txt", "r")
+    print(file.read())
+    divider()
+    file.close()
+except FileNotFoundError:
+    print("You forgot to make the file my boy :) or you typed the name wrong ;)")
+    divider()
