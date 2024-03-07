b1833a44baa127e403a3c8b2909f9c0fa394ad29
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-07 17:51:31 +0530

    Added command logger

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,22 +1,35 @@
 """This is programe to Manipulate files."""
 
-from typing import IO
+from typing import IO, Callable
 import time
+
+commands_dictionary: dict = {}
 
 def get_file(file_location: str, mode: str) -> IO[str]:
     try:
         file: IO[str] = open(file_location, mode)
         return file
     except FileExistsError:
-        return f"File on the path `{file_location}` is already there."
+        return f"File on the path '{file_location}' is already there."
     except FileNotFoundError:
-        return f"File on the path `{file_location}` does not exists."
+        return f"File on the path '{file_location}' does not exists."
     except Exception as e:
         return f"An error occured\nError: {e}"
 
 def file_size(file: IO[str]) -> int:
     size: int = file.tell()
     return size
+
+def command(name: str) -> None:
+
+    def decorator(func: Callable) -> Callable:
+        if name in commands_dictionary:
+            print(f"The command '{name}' can't be added twice.")
+            return
+        
+        commands_dictionary[name] = func.__name__
+
+    return decorator
 
 print("Welcome to File Manipulator Programe.")
 print("*" * 100)
