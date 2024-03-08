34780c3c75e539135246e5356529660e83148652
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-08 22:07:08 +0530

    Made it much readable

--- file_manipulator.py
+++ file_manipulator.py
@@ -3,12 +3,12 @@
 from typing import IO, Callable, Optional
 import os
 
+
 commands_dictionary = {}
 
+
 def command(name: str) -> Callable:
-
     def decorator(func: Callable) -> Callable:
-
         if name in commands_dictionary:
             print(f"The command '{name}' can't be added twice.")
             return
@@ -19,8 +19,10 @@ def command(name: str) -> Callable:
     
     return decorator
 
+
 def check_command(command: str) -> bool:
     return command.lower() in commands_dictionary
+
 
 def execute_command(command: str, *args: list[str]) -> None:
     if not check_command(command):
@@ -37,14 +39,17 @@ def execute_command(command: str, *args: list[str]) -> None:
     except Exception as e:
         print(f"Error: {e}")
 
+
 @command(name="help")
 def help_menu() -> None:
     for commands in commands_dictionary:
         print(commands)
 
+
 def get_file(file_location: str, file_mode: str) -> IO[str]:
     file: IO[str] = open(file_location, file_mode)
     return file
+
 
 @command(name="open")
 def open_file(file_location: str, file_mode: Optional[str] = "r") -> None:
@@ -52,21 +57,24 @@ def open_file(file_location: str, file_mode: Optional[str] = "r") -> None:
     opened_file = get_file(file_location, file_mode)
     print("File has been loaded.")
 
+
 @command(name="read")
 def read_file() -> None:
     print(opened_file.read())
+
 
 @command(name="close")
 def close_file() -> None:
     opened_file.close()
     print("File has been closed.")
 
+
 @command(name="clear")
 def clear_screen() -> None:
     os.system("cls" if os.name == "nt" else "clear")
 
-while True:
 
+while True:
     def get_command_from_user() -> str:
         command_input: str = input("Enter Command > ")
         return command_input
