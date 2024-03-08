b2bb0e6bc8d92debf4fb63a98580707637990b90
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-08 16:25:26 +0530

    Made a command decorator and it works

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,56 +1,45 @@
 """This is programe to Manipulate files."""
 
-from typing import IO, Callable
+from typing import IO, Callable, Optional
 
-commands_dictionary: dict = {}
+commands_dictionary = {}
 
-def get_file(file_location: str, mode: str) -> IO[str]:
-    try:
-        file: IO[str] = open(file_location, mode)
-        return file
-    except FileExistsError:
-        return f"File on the path '{file_location}' is already there."
-    except FileNotFoundError:
-        return f"File on the path '{file_location}' does not exist."
-    except Exception as e:
-        return f"An error occurred\nError: {e}"
-
-def file_size(file: IO[str]) -> int:
-    size: int = file.tell()
-    return size
-
-def command(name: str) -> None:
+def command(name: str) -> Callable:
 
     def decorator(func: Callable) -> Callable:
+
         if name in commands_dictionary:
             print(f"The command '{name}' can't be added twice.")
             return
         
         commands_dictionary[name] = func.__name__
-        return func
 
+        return func
+    
     return decorator
 
 def check_command(command: str) -> bool:
-    if not command.lower() in commands_dictionary:
-        return False
-    return True
-    
-def execute_command(command: str) -> None:
+    return command.lower() in commands_dictionary
+
+def execute_command(command: str, *args: list[str]) -> None:
     if not check_command(command):
-        print(f"Command '{command}' does not exist.")
+        print(f"Command '{command}' not found.")
         return
+    
     func_name: str = commands_dictionary[command.lower()]
+
     try:
-        func: Callable = globals()[func_name]
-        func()
+        func = globals()[func_name]
+        func(*args)
+    except ValueError as e:
+        print(f"Vakue Error: {e}")
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
 
-@command(name="help")
-def show_commands() -> None:
-    for name in commands_dictionary:
-        print(name)
+def get_command_from_user() -> str:
+    command_input: str = input("Enter Command > ")
+    return command_input
 
-user_command: str = input("> ")
-execute_command(user_command)
+command_from_user = get_command_from_user()
+
+execute_command(*command_from_user.split())
