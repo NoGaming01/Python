516191efd54210ebcfa56b92645d4b412bb8af40
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-10 23:15:34 +0530

    Made random_string_v2

--- completely_random_string.py
+++ completely_random_string.py
@@ -2,12 +2,30 @@
 
 import random
 import string
+from sys import getsizeof
 
 
 def random_string_v1(length: int) -> str:
-    characters = string.ascii_letters + string.digits
+    characters: str = string.ascii_letters + string.digits
     return "".join(random.choices(characters, k=length))
 
 
-while True:
-    print(random_string_v1(6))
+generated_tokens: list = []
+
+
+def random_string_v2(length: int) -> str:
+    characters: str = string.ascii_letters + string.digits
+
+    token = "".join(random.choices(characters, k=length))
+
+    while True:
+        if token in generated_tokens:
+            continue
+        generated_tokens.append(token)
+        return token
+        
+
+for i in range(10 ** 5):
+    print(random_string_v2(121))
+
+print(getsizeof(generated_tokens))
