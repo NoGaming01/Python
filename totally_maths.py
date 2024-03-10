753b226f218dabf84fcf6f0d202b8092ff6abd88
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-11 00:32:31 +0530

    Idk maths ig?

---
+++ totally_maths.py
@@ -0,0 +1,35 @@
+"""This is totally Mathematics"""
+
+import random
+import signal
+import sys
+
+
+def generate_problems() -> int:
+    number1: int = random.randint(100, 1000)
+    number2: int = random.randint(100, 1000)
+
+    return number1, number2
+
+
+def exit_shell(signal, frame):
+    print("\nExiting the shell.")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, exit_shell)
+
+
+while True:
+    number1, number2 = generate_problems()
+
+    result = number1 + number2
+
+    print(f"{number1} + {number2}")
+
+    user_input: int = int(input("> "))
+
+    if user_input == result:
+        print(f"You got it, {result=}")
+    else:
+        print(f"No, {result=}")
